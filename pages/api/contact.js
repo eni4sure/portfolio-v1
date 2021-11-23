64f15b76c66f4d66521a5bb47bd9e31e8413d048
Eniola Osabiya <eosabiya@gmail.com>
2021-11-24 00:07:41 +0100

    contact page check complete/approved

---
+++ pages/api/contact.js
@@ -0,0 +1,41 @@
+// Get Contact Form Data and send email with nodemailer
+const nodemailer = require("nodemailer");
+
+export default async function handler(req, res) {
+	const { name, email, subject, message } = req.body;
+
+	if (!name || !email || !subject || !message) {
+		return res.status(400).json({ error: "All fields are required" });
+	}
+
+	// Create transporter
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: process.env.NODEMAIL_CONTACT_EMAIL,
+			pass: process.env.NODEMAIL_CONTACT_PASSWORD,
+		},
+	});
+
+	// Create mail options
+	const mailOptions = {
+		from: email,
+		to: "eosabiya@gmail.com",
+		replyTo: email,
+		subject: `${subject} —  eniolaosabiya.com`,
+		text: message,
+		html: `<p>${message.replace(/(?:\r\n|\r|\n)/g, "<br>")}</p>`,
+	};
+
+	try {
+		// Send email
+		await transporter.sendMail(mailOptions, (err, info) => {
+			if (err) {
+				return res.status(500).json({ error: err.message });
+			}
+			return res.status(200).json({ message: "Email sent" });
+		});
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+}
--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import { TopNavigationBar, Footer } from "../app/components";
@@ -7,6 +8,42 @@ import locationContactIcon from "../public/assets/images/contact-page/icon-locat
 import emailContactIcon from "../public/assets/images/contact-page/icon-email.png";
 
 export default function Contact() {
+	const [contactFormData, setContactFormData] = React.useState({
+		name: "",
+		email: "",
+		subject: "",
+		message: "",
+	});
+	const [formState, setFormState] = React.useState({
+		submitted: false,
+		success: false,
+		message: null,
+	});
+
+	const handleContactFormSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch("/api/contact", {
+				method: "POST",
+				headers: {
+					Accept: "application/json, text/plain, */*",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(contactFormData),
+			});
+
+			if (response.status === 200) {
+				setFormState({ submitted: true, success: true });
+				setContactFormData({ name: "", email: "", subject: "", message: "" });
+				window.alert("Message sent successfully");
+			}
+		} catch (error) {
+			window.alert("Error Sending Message");
+			setFormState({ submitted: true, success: false, message: error.message });
+		}
+	};
+
 	return (
 		<>
 			<Head>
@@ -25,13 +62,64 @@ export default function Contact() {
 				<div className="contact-content">
 					<div className="contact-form">
 						<div className="form-header">Message Me</div>
-						<form className="myForm" onSubmit={(e) => e.preventDefault()}>
+						<form className="myForm" onSubmit={handleContactFormSubmit}>
 							<div className="input-line">
-								<input type="text" name="name" placeholder="Enter your Name" className="input-name" />
-								<input type="email" name="email" placeholder="Enter your Email" className="input-name" />
+								<input
+									type="text"
+									name="name"
+									required
+									placeholder="Enter your Name"
+									value={contactFormData.name}
+									onChange={(e) =>
+										setContactFormData({
+											...contactFormData,
+											name: e.target.value,
+										})
+									}
+									className="input-name"
+								/>
+								<input
+									type="email"
+									name="email"
+									required
+									placeholder="Enter your Email"
+									value={contactFormData.email}
+									onChange={(e) =>
+										setContactFormData({
+											...contactFormData,
+											email: e.target.value,
+										})
+									}
+									className="input-name"
+								/>
 							</div>
-							<input type="text" name="subject" placeholder="Subject of message" className="input-subject" />
-							<textarea name="message" className="input-textarea" placeholder="Enter your message..." />
+							<input
+								type="text"
+								name="subject"
+								required
+								placeholder="Subject of message"
+								value={contactFormData.subject}
+								onChange={(e) =>
+									setContactFormData({
+										...contactFormData,
+										subject: e.target.value,
+									})
+								}
+								className="input-subject"
+							/>
+							<textarea
+								name="message"
+								className="input-textarea"
+								required
+								placeholder="Enter your message..."
+								value={contactFormData.message}
+								onChange={(e) =>
+									setContactFormData({
+										...contactFormData,
+										message: e.target.value,
+									})
+								}
+							/>
 							<button type="submit" id="submit">
 								Submit
 							</button>
