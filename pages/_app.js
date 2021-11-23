3eed496dfbf5c213006cc75bfe9c4943b658dd58
Eniola Osabiya <eosabiya@gmail.com>
2021-11-23 13:57:46 +0100

    head seo tags added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				{/* <!-- Primary Meta Tags --> */}
+				<title>Eniola Osabiya - @eni4sure</title>
+				<meta name="title" content="Eniola Osabiya - @eni4sure" />
+				<meta name="description" content="17yo FullStack Dev, #TeensInTech Advocate, DevOps Engineer" />
+
+				{/* Favicon */}
+				<link rel="icon" type="image/png" href="/favicon.ico" />
+
+				{/* <!-- Open Graph / Facebook --> */}
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://eniolaosabiya.com/" />
+				<meta property="og:title" content="Eniola Osabiya - @eni4sure" />
+				<meta property="og:description" content="17yo FullStack Dev, #TeensInTech Advocate, DevOps Engineer" />
+				{/* <meta property="og:image" content="https://eniolaosabiya.com/assets/images/" /> */}
+
+				{/* <!-- Twitter --> */}
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://eniolaosabiya.com/" />
+				<meta property="twitter:title" content="Eniola Osabiya - @eni4sure" />
+				<meta property="twitter:description" content="17yo FullStack Dev, #TeensInTech Advocate, DevOps Engineer" />
+				{/* <meta property="twitter:image" content="https://eniolaosabiya.com/assets/images/" /> */}
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
