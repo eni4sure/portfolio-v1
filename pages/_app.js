09850ecf65a1e8b64fcb301ac98cd4edc65913bd
Eniola Osabiya <eosabiya@gmail.com>
2021-12-06 10:24:06 +0100

    added seo og:updated_time

--- pages/_app.js
+++ pages/_app.js
@@ -45,6 +45,8 @@ function MyApp({ Component, pageProps }) {
 				<meta property="og:title" content="Eniola Osabiya || @eni4sure" />
 				<meta property="og:description" content="17yo FullStack Dev, #TeensInTech Advocate, DevOps Engineer" />
 				<meta property="og:image" content="https://www.eniolaosabiya.com/assets/site-preview.png" />
+				<meta property="og:updated_time" content="2021-12-06T09:23:21.056Z" />
+				{/* <meta property="og:updated_time" content={new Date().toISOString()} /> */}
 
 				{/* <!-- Twitter --> */}
 				<meta property="twitter:card" content="summary_large_image" />
@@ -54,7 +56,7 @@ function MyApp({ Component, pageProps }) {
 				<meta property="twitter:image" content="https://www.eniolaosabiya.com/assets/site-preview.png" />
 			</Head>
 
-			{isLoading ? <div className="page-switch-animation" /> : <Component pageViews={pageViews} {...pageProps} />}
+			<>{isLoading ? <div className="page-switch-animation" /> : <Component pageViews={pageViews} {...pageProps} />}</>
 		</>
 	);
 }
