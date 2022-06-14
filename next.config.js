20b36560ab5867aa202262c166e2a992508e6742
Eniola Osabiya <eosabiya@gmail.com>
2022-06-14 02:52:37 +0100

    feat: resume redirect added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "https://drive.google.com/file/d/1Y_eJk_k1tW7_Q6TGSzBVIjz4YGYZjWh5/view?usp=sharing",
+        permanent: false,
+      },
+    ];
+  },
 }
